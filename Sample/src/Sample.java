07ae9abe0b8429fa4c72a0759b33829631fcf978
hvaranasi <hvaranasi@GUEST034.corp.miraclesoft.com>
2018-12-06 10:06:22 +0530

    Committed latest code

--- Sample/src/Sample.java
+++ Sample/src/Sample.java
@@ -1,7 +1,7 @@
 public class Sample {
-	public static void main(String args[])
+	public static void main(string args[])
 	{
-		System.out.println("Hello Kalyan");
+		System.out.println("Hello Java");
 	}
 
 }
